ea4a4bc593126c3af5e41cb275b1b049b92388d9
Rafael Ortega <>
2018-05-22 14:49:40 +0200

    initial flutter project

---
+++ lib/main.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(new MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      title: 'Flutter-Native Demo',
+      theme: new ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: new MyHomePage(title: 'Channel demo app'),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget{
+
+  final String title;
+
+  const MyHomePage({Key key, this.title}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text(title),
+      ),
+      body: new Center(
+        child: new Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            new RaisedButton(
+              child: new Text('Show native view'),
+              onPressed: _showNativeView,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  void _showNativeView() {
+
+  }
+}
